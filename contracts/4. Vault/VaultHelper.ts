abfde3cd0ef2ea4cdd47935a524b6ea11dee66ea
Justin Phu <me@justinphu.dev>
2022-05-21 10:18:18 -0500

    feat: complete parts of hack 4
    Not entirely working due to upstream bug. I'll look into this later.

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -2,7 +2,12 @@ import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
+  // Slot 0 has the boolean.
+  // Slot 1 has the password!
+  // TODO(jqphu): due to https://github.com/ethers-io/ethers.js/issues/2982 this won't work. Downgrade hardhat to get this test to pass.
+  const result = await ethers.provider.getStorageAt(victim.address, 1);
+  // TODO(jqphu): 
+  /*
     Add code here that will help you pass the test
     Note: Unlock without using the string "A very strong password"
     Unlock the vault by somehow reading the private password from 
